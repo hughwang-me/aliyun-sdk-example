8418bb4d3351e2ce94d0508a368891ab39180b00
Hugh Wang <18501667737@163.com>
2019-09-18 15:24:33 +0800

    by Hugh

---
+++ src/main/java/com/uwjx/aliyunsdk/AliyunSdkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.uwjx.aliyunsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliyunSdkExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliyunSdkExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uwjx/aliyunsdk/AliyunSdkExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uwjx.aliyunsdk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AliyunSdkExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
